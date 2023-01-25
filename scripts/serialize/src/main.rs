6a684db70328f61cff971da917a547b1cfa392f1
lubtd <lucas.bertrand.22@gmail.com>
2023-01-25 23:36:58 +0100

    feat(script): serialize propose instantiation

--- scripts/serialize/src/main.rs
+++ scripts/serialize/src/main.rs
@@ -1,10 +1,17 @@
-use cosmwasm_std::to_binary;
+use cosmwasm_std::{to_binary, Decimal, Empty, Uint128};
+use dao_interface::{Admin, ModuleInstantiateInfo};
+use dao_voting::{
+    deposit::{DepositRefundPolicy, DepositToken, UncheckedDepositInfo},
+    pre_propose::PreProposeInfo,
+    threshold::PercentageThreshold,
+    threshold::Threshold,
+};
 
 fn main() {
     // Get binary of a voting contract instantiate message
-    let bin = to_binary(&dao_voting_cw20_staked::msg::InstantiateMsg {
+    let voting_bin = to_binary(&dao_voting_cw20_staked::msg::InstantiateMsg {
         token_info: dao_voting_cw20_staked::msg::TokenInfo::Existing {
-            address: "juno182jzjwdyl5fw43yujnlljddgtrkr04dpd30ywp2yn724u7qhtaqsnwt2ks".to_string(),
+            address: "juno1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrsf8smqw".to_string(),
             staking_contract: dao_voting_cw20_staked::msg::StakingInfo::New {
                 staking_code_id: 10,
                 unstaking_duration: None,
@@ -13,5 +20,36 @@ fn main() {
         active_threshold: None,
     });
 
-    println!("{}", bin.unwrap());
+    // Get binary of propose contract instantiate message
+    let propose_bin = to_binary(&dao_proposal_single::msg::InstantiateMsg {
+        min_voting_period: None,
+        threshold: Threshold::ThresholdQuorum {
+            threshold: PercentageThreshold::Majority {},
+            quorum: PercentageThreshold::Percent(Decimal::percent(10)),
+        },
+        max_voting_period: cw_utils::Duration::Time(432000),
+        allow_revoting: false,
+        only_members_execute: true,
+        pre_propose_info: PreProposeInfo::ModuleMayPropose {
+            info: ModuleInstantiateInfo {
+                code_id: 10,
+                msg: to_binary(&dao_pre_propose_single::InstantiateMsg {
+                    deposit_info: Some(UncheckedDepositInfo {
+                        denom: DepositToken::VotingModuleToken {},
+                        amount: Uint128::new(1000000000),
+                        refund_policy: DepositRefundPolicy::OnlyPassed,
+                    }),
+                    open_proposal_submission: false,
+                    extension: Empty::default(),
+                })
+                .unwrap(),
+                admin: Some(Admin::CoreModule {}),
+                label: "DAO DAO Pre-Propose Module".to_string(),
+            },
+        },
+        close_proposal_on_execution_failure: false,
+    });
+
+    println!("voting: {}", voting_bin.unwrap());
+    println!("propose: {}", propose_bin.unwrap());
 }
