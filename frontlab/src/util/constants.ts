cae78259097bc6aa031d0771b35712e6d88edebe
lubtd <lucas.bertrand.22@gmail.com>
2023-01-27 18:03:54 +0100

    chore: remove unused files

--- frontlab/src/util/constants.ts
+++
@@ -1,4 +0,0 @@
-export const GAS_PRICE = "0.0025ujunox";
-export const DEFAULT_REFETCH_ON_WINDOW_FOCUS_STALE_TIME = 15000;
-export const CHAIN_ID = "testing";
-export const CHAIN_RPC = "http://localhost:26657";
