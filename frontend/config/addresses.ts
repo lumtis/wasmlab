467970453a63d716c6572ce3e3a7ac1db91f858c
lubtd <lucas.bertrand.22@gmail.com>
2023-02-08 16:19:04 +0100

    chore: new addresses

--- frontend/config/addresses.ts
+++ frontend/config/addresses.ts
@@ -2,6 +2,6 @@
 export const addresses = {
   fooCW20: "juno1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrsf8smqw",
   fooMinter: "juno1wug8sewp6cedgkmrmvhl3lf3tulagm9hnvy8p0rppz9yjw0g4wtqwrw37d",
-  dao: "juno17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgszu8fr9",
-  staking: "juno13ehuhysn5mqjeaheeuew2gjs785f6k7jm8vfsqg3jhtpkwppcmzq7d63zd",
+  dao: "juno1qwlgtx52gsdu7dtp0cekka5zehdl0uj3fhp9acg325fvgs8jdzks9z8n5r",
+  staking: "juno10qt8wg0n7z740ssvf3urmvgtjhxpyp74hxqvqt7z226gykuus7eqs24840",
 };
